4ac50b07046167b9eca812ad1ccf5d4d478b2f90
Igor Tsupko <i@tsupko.tech>
2020-08-10 18:24:59 +0300

    werf guide nodejs assets

--- applications-guide/gitlab-nodejs/examples/040-assets/webpack.config.js
+++ applications-guide/gitlab-nodejs/examples/040-assets/webpack.config.js
@@ -3,14 +3,6 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html"),
