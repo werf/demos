550be59c220cd40eae03d1884f9008b9ebfb0889
Igor Tsupko <i@tsupko.tech>
2020-08-20 14:56:08 +0300

    werf guide

--- applications-guide/gitlab-java-springboot/040-assets/assets/webpack.config.js
+++ applications-guide/gitlab-java-springboot/040-assets/assets/webpack.config.js
@@ -1,10 +1,16 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'development'
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "src/config", to: 'config' }
+            ]
+        }),
+    ],
 };
