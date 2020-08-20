481f4071d6f72696913f71703b01071c47f442c2
Igor Tsupko <i@tsupko.tech>
2020-08-20 15:50:11 +0300

    revert

--- applications-guide/gitlab-java-springboot/040-assets/assets/webpack.config.js
+++ applications-guide/gitlab-java-springboot/040-assets/assets/webpack.config.js
@@ -1,16 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html"),
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: "src/config", to: 'config' }
-            ]
-        }),
-    ],
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development'
 };
